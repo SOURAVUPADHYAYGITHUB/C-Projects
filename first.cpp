5ad18dc0c5aa5c2ebf4713e349669cc0929c0e95
Sourav <128739585+SOURAVUPADHYAYGITHUB@users.noreply.github.com>
2025-01-21 12:48:29 +0530

    Update first.cpp

--- first.cpp
+++ first.cpp
@@ -1,7 +1,7 @@
-#include <iostream>
-int main()
-{
-    std::cout << "Hello World!" << std::endl;
-    return 0;
+#include <iostream>  // Include the input-output stream library
 
+int main()  // Main function where the program execution begins
+{
+    std::cout << "Hello World!" << std::endl;  // Print "Hello World!" to the console
+    return 0;  // Return 0 to indicate that the program ended successfully
 }
